65b23d54a98f973802ecae85fdee71ccb87c0261
Wyf0412 <3551364965@qq.com>
2026-05-13 14:06:48 +0800

    Update TicketDetail.tsx

--- src/pages/TicketDetail.tsx
+++ src/pages/TicketDetail.tsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useTicketStore } from "../store/ticketStore";
 import { useEffect } from "react";
 
